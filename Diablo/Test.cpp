53f3b7ef59d93ae6800cef25928dc47fece194f9
RAFAELPUTARO <rafaelputaro@gmail.com>
2013-03-28 11:09:49 -0300

    Agregue una clase Test
    La idea es ir metiendo los test sobre las clases individuales en esta
    clase para que el main quede libre y así evitar conflictos al hacer
    commit, ya que hacemos un merge sobre el main o Test y de última se
    rompe algo que no es importante.

---
+++ Diablo/Test.cpp
@@ -0,0 +1,13 @@
+#include "Test.h"
+	
+	//Todos los tests
+	bool Test::test() {
+		bool retorno = True;
+		retorno = testImagenEstatica()&& retorno;
+		return retorno;
+	}
+
+	//Test Imagen Estatica
+	bool Test::testImagenEstatica() {
+		
+	}
---
+++ Diablo/Test.h
@@ -0,0 +1,19 @@
+#pragma once
+#include "ImagenEstatica.h"
+class Test
+{
+private:
+	
+	Test(void);
+	
+	~Test(void);
+
+public:
+
+	//Todos los tests
+	static bool test();
+
+	//Test Imagen Estatica
+	static bool testImagenEstatica();
+};
+

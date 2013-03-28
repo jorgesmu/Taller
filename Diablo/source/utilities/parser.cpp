dc14de9c61ee4b927f3d069e3f5868ba90cd7539
Jorge Smulevici <jorgsmu@gmail.com>
2013-03-28 18:35:05 -0300

    Se agrega prueba de leer con Yaml un escalar y mostrarlo en la salida.

--- Diablo/source/utilities/parser.cpp
+++ Diablo/source/utilities/parser.cpp
@@ -1,14 +1,43 @@
 #include "parser.h"
+#include <iostream>
+#include <fstream>
 
-//Intenta leer un archivo de prueba
+using namespace std;
+//lee e imprime un archivo
+void imprimir_documento(char* path){
+		//se puede mejorar pero es solo una clase de test
+		cout << endl << "Contenido del archivo:" << path << endl;
+		char cadena[100];
+		std::ifstream archivo(path);
+		if (archivo.good()){
+		
+			while (!archivo.eof()){
+				archivo >> cadena;
+				cout<< cadena <<endl;
+			}
+
+		}
+		archivo.close();
+		return;
+}
 int parser_test() {
-	std::ifstream fin("test.yml");
-    YAML::Parser parser(fin);
+	//ejemplos tomados de https://code.google.com/p/yaml-cpp/wiki/HowToParseADocument
+	try{
+		cout << "Comienza Yaml parsing." << endl;
+		char* path="../resources/levels/test.yaml";
+		imprimir_documento(path);
+		
+		std::ifstream archivo(path);
+		YAML::Parser parser(archivo);
+		YAML::Node doc;
 
-    YAML::Node doc;
-    while(parser.GetNextDocument(doc)) {
-       
-    }
-	
-    return 0;
+		parser.GetNextDocument(doc);
+		std::string unValor;
+		doc >> unValor;
+		std::cout << endl << "El valor del escalar tomado con Yaml: " << unValor << std::endl;
+
+	}catch (exception e){
+		cout << "Ocurrio un error al utilizar la Yaml";
+	}
+		return 0;
 }

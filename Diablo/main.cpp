c03db1c8ebe0c5ca0a013927deddf59501bc69fe
Jorge Smulevici <jorgsmu@gmail.com>
2013-03-29 22:03:45 -0300

    Se agrega el ejemplo de parsear un vector de 3 dimensiones con Yaml. Se comento la linea de test hecha por Rafa ya que traia inconvenientes.
    Aclaracion: el codigo escrito hasta ahora son adaptaciones de los
    ejemplos que se encuentran en la pagina. Se hacen los ejemplos completos
    y se agregan couts para poder verificar facilmente la salida, tambien.
    se traducen algunas variables para hacer mas facil su comprension

--- Diablo/main.cpp
+++ Diablo/main.cpp
@@ -85,7 +85,7 @@ int main(int argc, char* args[]) {
 
 	}
 
-	Test::test();
+	//Test::test();
 	
 	surf.destroy();
 
--- Diablo/source/utilities/parser.cpp
+++ Diablo/source/utilities/parser.cpp
@@ -3,6 +3,16 @@
 #include <fstream>
 
 using namespace std;
+//estrutura de un vector de 3 dimensiones para correr los ejemplos de yaml-cpp
+struct Vector_3_dim {
+    float x, y, z;
+};
+void operator >> (const YAML::Node& node, Vector_3_dim& v)
+{
+    node[0] >> v.x;
+    node[1] >> v.y;
+    node[2] >> v.z;
+}
 //lee e imprime un archivo
 void imprimir_documento(char* path){
 		//se puede mejorar pero es solo una clase de test
@@ -19,6 +29,29 @@ void imprimir_documento(char* path){
 		}
 		archivo.close();
 		return;
+}
+void parser_test_vector(){
+	char* path="../resources/levels/test4.yml";
+	imprimir_documento(path);
+
+
+	cout << endl << "Test parsear vector." << endl << "Contenido parseado con Yaml: " << endl;
+
+	std::ifstream archivo(path);
+	YAML::Parser parser(archivo);
+	YAML::Node doc;    
+	parser.GetNextDocument(doc);
+
+	Vector_3_dim unVector;
+	doc >> unVector;
+	
+	std::cout << "El valor de las componentes del vector es: (" << unVector.x << ", " << unVector.y << ", " << unVector.z << ")";
+
+	archivo.close();
+
+	
+
+	return;
 }
 void parser_test_arrays(){
 	char* path = "../resources/levels/test3.yml";
@@ -64,19 +97,19 @@ void parser_test_lista() {
 }
 void parser_test_escalar() {
 
-		char* path="../resources/levels/test.yaml";
-		imprimir_documento(path);
+	char* path="../resources/levels/test.yaml";
+	imprimir_documento(path);
 
-		std::ifstream archivo(path);
-		YAML::Parser parser(archivo);
-		YAML::Node doc;
+	std::ifstream archivo(path);
+	YAML::Parser parser(archivo);
+	YAML::Node doc;
 
-		parser.GetNextDocument(doc);
-		std::string unValor;
-		doc >> unValor;
-		std::cout << endl << "Test parsear escalar." << endl << "El valor del escalar tomado con Yaml: " << unValor << std::endl;
+	parser.GetNextDocument(doc);
+	std::string unValor;
+	doc >> unValor;
+	std::cout << endl << "Test parsear escalar." << endl << "El valor del escalar tomado con Yaml: " << unValor << endl;
 
-		archivo.close();
+	archivo.close();
 
 
 
@@ -84,13 +117,19 @@ void parser_test_escalar() {
 }
 void parser_test(){
 	//ejemplos tomados y basados en: https://code.google.com/p/yaml-cpp/wiki/HowToParseADocument
+	/* Aclaracion: el codigo escrito hasta ahora son adaptaciones de los ejemplos que se encuentran en la pagina.
+	   se hacen los ejemplos completos y se agregan couts para poder verificar facilmente la salida, tambien
+	   se traducen algunas variables para hacer mas facil su comprension*/
 
 	cout << "Comienza Yaml parsing." << endl;
 	try{
 		parser_test_escalar();
 		parser_test_lista();
 		parser_test_arrays();
+		parser_test_vector();
 	}catch (exception e){
 		cout << endl << endl << "Ocurrio un error con la Yaml";
 	}
+
+	return;
 }

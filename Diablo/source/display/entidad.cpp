ebde9b90681c6d70bb97b5a243dbd72cad95e86a
rafaelputaro <rafaelputaro@gmail.com>
2013-03-31 19:28:47 -0300

    Entidad sin probar

--- Diablo/source/display/entidad.cpp
+++ Diablo/source/display/entidad.cpp
@@ -1,18 +1,185 @@
 #include "entidad.h"
 
-Entidad::Entidad(){
-}
+	/*
+		Pre:-
+		 
+		Post: Se ha inicializado la instancia con los atributos x, y, w y h en cero, surf e 
+		imagenPersonaje en NULL, y name y path como cadenas vacías.
+	*/
+	Entidad::Entidad(){
+		this -> h = 0;
+		this -> w = 0;
+		this -> x = 0;
+		this -> y = 0;
+		this -> name = "";
+		this -> path = "";
+		this -> surf = NULL;
+		this -> imagen = NULL;
+	}
 
-void Entidad::init(const std::string& name, const std::string& img){
-	bool load;
-	this -> name = name;
-	this -> imagen = img;
-	//Surface surface;
-	//load = surface.load("../resources/tile.bmp" , Surface::RGB_AZUL);
-	this -> surf.load("../resources/tile.bmp" , Surface::RGB_AZUL);;
-}
+	/*
+		Pre:-
+		 
+		Post: Se ha inicializado la instancia segun el archivo y los parámetros.
 
-void Entidad::blit(SDL_Surface* dest, const SDL_Rect& cam){
-	//Blitting
-	surf.blit(dest, cam.x, cam.y);
-}
+		NOTA: ImagenAnimada
+	*/
+	Entidad::Entidad(const std::string& name, const std::string& path , const unsigned int w , 
+					const unsigned int h , const int fps , const int delay) {
+		this -> imagen	= new ImagenAnimada(path.c_str() , this -> h , this -> w , fps , 
+														delay , Imagen::COLOR_KEY);
+		this -> name = name;
+		this -> path = this -> imagen -> getPath();
+		this -> x = 0;
+		this -> y = 0;
+		this -> w = this -> imagen -> getAncho(); 
+		this -> h = this -> imagen -> getAlto();
+		this -> surf = this -> imagen -> getSurface();
+	}
+
+	/*
+		Pre:-
+		 
+		Post: Se ha inicializado la instancia segun el archivo y los parámetros.
+
+		NOTA: ImagenAnimada
+	*/
+	Entidad::Entidad(const std::string& name, const std::string& path , const unsigned int w , 
+					const unsigned int h , const int fps , const int delay , const int colorKey) {
+		this -> imagen	= new ImagenAnimada(path.c_str() , this -> h , this -> w , fps , delay , colorKey);
+		this -> name = name;
+		this -> path = this -> imagen -> getPath();
+		this -> x = 0;
+		this -> y = 0;
+		this -> w = this -> imagen -> getAncho(); 
+		this -> h = this -> imagen -> getAlto();
+		this -> surf = this -> imagen -> getSurface();
+	}
+
+	/*
+		Pre:
+
+		Post:
+
+		NOTA: ImagenAnimada
+	*/
+	void Entidad::init(const std::string& name , const std::string& path , const unsigned int w , 
+						const unsigned int h , const int fps , const int delay){
+		if (this -> imagen != NULL) {
+			delete(this -> imagen);
+			this -> imagen = NULL;
+		}
+		this -> imagen	= new ImagenAnimada(path.c_str() , this -> h , this -> w , fps , 
+														delay , Imagen::COLOR_KEY);
+		this -> name = name;
+		this -> path = this -> imagen -> getPath();
+		this -> x = 0;
+		this -> y = 0;
+		this -> w = this -> imagen -> getAncho(); 
+		this -> h = this -> imagen -> getAlto();
+		this -> surf = this -> imagen -> getSurface();
+	}
+
+	/*
+		Pre:
+
+		Post:
+
+		NOTA: ImagenAnimada
+	*/
+	void Entidad::init(const std::string& name , const std::string& path , const unsigned int w , 
+						const unsigned int h , const int fps , const int delay , const int colorKey){
+		if (this -> imagen != NULL) {
+			delete(this -> imagen);
+			this -> imagen = NULL;
+		}
+		this -> imagen	= new ImagenAnimada(path.c_str() , this -> h , this -> w , fps , 
+														delay , Imagen::COLOR_KEY);
+		this -> name = name;
+		this -> path = this -> imagen -> getPath();
+		this -> x = 0;
+		this -> y = 0;
+		this -> w = this -> imagen -> getAncho(); 
+		this -> h = this -> imagen -> getAlto();
+		this -> surf = this -> imagen -> getSurface();
+	}
+
+	/*
+		Pre:
+
+		Post:
+
+		NOTA: ImagenEstatica
+	*/
+	void Entidad::init(const std::string& name , const std::string& path){
+		if (this -> imagen != NULL) {
+			delete(this -> imagen);
+			this -> imagen = NULL;
+		}
+		this -> imagen	= new ImagenEstatica(path.c_str() , Imagen::COLOR_KEY);
+		this -> name = name;
+		this -> path = this -> imagen -> getPath();
+		this -> x = 0;
+		this -> y = 0;
+		this -> w = this -> imagen -> getAncho(); 
+		this -> h = this -> imagen -> getAlto();
+		this -> surf = this -> imagen -> getSurface();
+	}
+
+	/*
+		Pre:
+
+		Post:
+
+		NOTA: ImagenEstatica
+	*/
+	void Entidad::init(const std::string& name , const std::string& path , const int colorKey){
+		if (this -> imagen != NULL) {
+			delete(this -> imagen);
+			this -> imagen = NULL;
+		}
+		this -> imagen	= new ImagenEstatica(path.c_str() , colorKey);
+		this -> name = name;
+		this -> path = this -> imagen -> getPath();
+		this -> x = 0;
+		this -> y = 0;
+		this -> w = this -> imagen -> getAncho(); 
+		this -> h = this -> imagen -> getAlto();
+		this -> surf = this -> imagen -> getSurface();
+	}
+
+	/*
+		Pre: La instancia ha sido creada.
+		 
+		Post: Se ha destruido la instancia liberando los recursos asociados.
+	*/
+	Entidad::~Entidad() {
+		if (this -> imagen != NULL) {
+			delete(this -> imagen);
+			this -> imagen = NULL;
+		}
+		this -> surf = NULL;
+	}
+
+	// Cambia la posicion de la entidad
+	void Entidad::mover(int x, int y) {
+
+	}
+	
+	// Getters para la posicion
+	int Entidad::getX() const {
+		return this -> x;
+	}
+	int Entidad::getY() const{
+		return this -> y;
+	}
+	
+	// Actualiza las cosas internas, si hubiese
+	void Entidad::update() {
+		this -> surf = this -> imagen -> getSurface();
+	}
+	
+	// Dibuja la entidad
+	void Entidad::blit(SDL_Surface* dest, SDL_Rect& cam){
+		(this -> surf) -> blit(dest , this -> x , this -> y , cam);
+	}
--- Diablo/source/display/entidad.h
+++ Diablo/source/display/entidad.h
@@ -1,29 +1,133 @@
+#pragma	once
+
 #include <string>
 #include "../utilities/surface.h"
+#include "../utilities/Imagen.h"
+#include "../utilities/ImagenEstatica.h"
+#include "../utilities/ImagenAnimada.h"
 #include "SDL.h"
 
 class Entidad {
 
 	private:
-	std::string name; // Nombre de la entidad
-	std::string imagen; // Path a la imagen
-	int x, y; // Posicion en la grilla
-	Surface surf; // Puntero a la surface de esta entidad (traida desde el resource manager on load)
-	int w, h; // Ancho y alto de la base en la grilla
+		
+		std::string name; // Nombre de la entidad
+		
+		std::string path; // Path a la imagen
+		
+		int x, y; // Posicion en la grilla
+		
+		Surface* surf; // Puntero a la surface de esta entidad (traida desde el resource manager on load)
+		
+		Imagen* imagen;
+
+		int w, h; // Ancho y alto de la base en la grilla
 
 	public:
-	// Ctor
+	
+	/*
+		Pre:-
+		 
+		Post: Se ha inicializado la instancia con los atributos x, y, w y h en cero, surf e 
+		imagen en NULL, y name y path como cadenas vacías.
+	*/
 	Entidad();
-	// Settea el nombre y carga la surface
-	void init(const std::string& name, const std::string& img);
+
+	/*
+		Pre:-
+		 
+		Post: Se ha inicializado la instancia segun el archivo y los parámetros.
+
+		NOTA: ImagenEstatica
+	*/
+	Entidad(const std::string& name, const std::string& path);
+
+	/*
+		Pre:-
+		 
+		Post: Se ha inicializado la instancia segun el archivo y los parámetros.
+
+		NOTA: ImagenEstatica
+	*/
+	Entidad(const std::string& name , const std::string& path , const int colorKey);
+
+	/*
+		Pre:-
+		 
+		Post: Se ha inicializado la instancia segun el archivo y los parámetros.
+
+		NOTA: ImagenEstatica
+	*/
+	void init(const std::string& name , const std::string& path);
+
+	/*
+		Pre:-
+		 
+		Post: Se ha inicializado la instancia segun el archivo y los parámetros.
+
+		NOTA: ImagenEstatica
+	*/
+	void init(const std::string& name , const std::string& path , const int colorKey);
+
+	/*
+		Pre:-
+		 
+		Post: Se ha inicializado la instancia segun el archivo y los parámetros.
+
+		NOTA: ImagenAnimada
+	*/
+	Entidad(const std::string& name, const std::string& path , const unsigned int w , 
+				const unsigned int h , const int fps , const int delay);
+
+	/*
+		Pre:-
+		 
+		Post: Se ha inicializado la instancia segun el archivo y los parámetros.
+
+		NOTA: ImagenAnimada
+	*/
+	Entidad(const std::string& name, const std::string& path , const unsigned int w , 
+				const unsigned int h , const int fps , const int delay , const int colorKey);
+
+	/*
+		Pre:
+
+		Post:
+
+		NOTA: ImagenAnimada
+	*/
+	void init(const std::string& name, const std::string& path , const unsigned int w , 
+				const unsigned int h , const int fps , const int delay);
+	
+	/*
+		Pre:
+
+		Post:
+
+		NOTA: ImagenAnimada
+	*/
+	void init(const std::string& name, const std::string& path , const unsigned int w , 
+				const unsigned int h , const int fps , const int delay , const int colorKey);
+		
+	/*
+		Pre: La instancia ha sido creada.
+		 
+		Post: Se ha destruido la instancia liberando los recursos asociados.
+	*/
+	~Entidad();
+		
 	// Cambia la posicion de la entidad
 	void mover(int x, int y);
+	
 	// Getters para la posicion
 	int getX() const;
+	
 	int getY() const;
 	// Actualiza las cosas internas, si hubiese
+	
 	void update();
+	
 	// Dibuja la entidad
-	void blit(SDL_Surface* dest, const SDL_Rect& cam);
+	void blit(SDL_Surface* dest, SDL_Rect& cam);
 
 };

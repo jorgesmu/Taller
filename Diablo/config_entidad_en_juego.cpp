3368f0ce28326144eb42a57916a5ab2775b87a58
Jorge Smulevici <jorgsmu@gmail.com>
2013-04-03 07:07:52 -0300

    Se agrega la clase config_entidad_en_juego necesaria para parsear un escenario.

---
+++ Diablo/config_entidad_en_juego.cpp
@@ -0,0 +1,8 @@
+#include "config_entidad_en_juego.h"
+
+//Constructor
+config_entidad_en_juego::config_entidad_en_juego() {
+	nombre = "";
+	pos_x = -1;
+	pos_y = -1;
+};
---
+++ Diablo/config_entidad_en_juego.h
@@ -0,0 +1,31 @@
+#pragma once
+
+#include <string>
+using namespace std;
+class config_entidad_en_juego {
+
+	private:
+		string nombre;
+		int pos_x,pos_y;
+
+	public:
+		//Constructor
+		//i: inicial
+		config_entidad_en_juego();
+
+		//Getters
+		string get_nombre() { return nombre;}
+
+		int get_pos_x() { return pos_x;}
+
+		int get_pos_y() { return pos_y;}
+
+		//Setters
+		void set_nombre(string newValue) { nombre = newValue;}
+
+		void set_pos_x(int newValue) { pos_x = newValue;}
+
+		void set_pos_y(int newValue) { pos_y = newValue;}
+
+
+};

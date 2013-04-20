83267cdbd60a6e0874a9157f011ad4c0bdd748f6
vnportnoy <vnportnoy@gmail.com>
2013-04-20 16:29:16 -0300

    Clases Cliente y Servidor creadas

---
+++ Diablo/Cliente/cliente.cpp
@@ -0,0 +1,110 @@
+#include "cliente.h"
+
+Cliente::Cliente() {
+	this->result=NULL;
+	this->ptr=NULL;
+}
+
+//Inicializa Winsock
+int inicializar() {
+	WSADATA wsaData;
+	int res=WSAStartup(MAKEWORD(2,2),&wsaData);
+	if (res!=0) {
+		printf("Inicializacion Winsock fallida: %d \n",res);
+		return 1;
+	}
+	printf("Winsock inicializado \n");
+	return 0;
+}
+
+//Obtiene la direccion del host a partir de los datos
+int Cliente::obtenerDireccion(char *dir,char* port) {
+	inicializar();
+	ZeroMemory(&hints,sizeof(hints));
+	hints.ai_family=AF_UNSPEC;
+	hints.ai_socktype=SOCK_STREAM;
+	hints.ai_protocol=IPPROTO_TCP;
+
+	int resultado=getaddrinfo(dir,port,&hints,&result);
+	if (resultado!=0) {
+		printf("Obtencion de direccion fallida: %d\n",resultado);
+		WSACleanup();
+		return 1;
+	}
+	printf("Direccion del host obtenida \n");
+	return 0;
+}
+
+//Crea un nuevo socket para establecer la conexion
+int Cliente::crearSocket() {
+	ConnectSocket=INVALID_SOCKET;
+	ptr=result;
+	ConnectSocket=socket(ptr->ai_family,ptr->ai_socktype,ptr->ai_protocol);
+	if (ConnectSocket==INVALID_SOCKET) {
+		printf("Error al crear socket: %ld\n",WSAGetLastError());
+		freeaddrinfo(result);
+		WSACleanup();
+		return 1;
+	}
+	printf("Socket creado \n");
+	return 0;
+}
+
+//Conecta el socket con el host
+int Cliente::conectar() {
+	int resultado=connect(ConnectSocket,ptr->ai_addr,(int)ptr->ai_addrlen);
+	if (resultado==SOCKET_ERROR) {
+		printf("Error al intentar conectar al servidor\n");
+		closesocket(ConnectSocket);
+		ConnectSocket=INVALID_SOCKET;
+		return 1;
+	}
+	freeaddrinfo(result);
+	if (ConnectSocket==INVALID_SOCKET) {
+		printf("Error de conexion-Socket invalido\n");
+		WSACleanup();
+		return 1;
+	}
+	printf("Conexion establecida \n");
+	return 0;
+}
+
+//Envia datos al host
+int Cliente::enviar(char* data,int len) {
+	int resultado=send(ConnectSocket,data,len,0);
+	if (resultado==SOCKET_ERROR) {
+		printf("Error al enviar data: %d\n",WSAGetLastError());
+		closesocket(ConnectSocket);
+		WSACleanup();
+		return 1;
+	}
+
+	printf("Bytes enviados: %ld\n",resultado);
+	printf("Mensaje enviado: %s \n",data);
+
+	//Dejo de enviar
+	resultado=shutdown(ConnectSocket,SD_SEND);
+	if (resultado==SOCKET_ERROR) {
+		printf("Termino de envio fallido: %d\n",WSAGetLastError());
+		closesocket(ConnectSocket);
+		WSACleanup();
+		return 1;
+	}
+
+	printf("Proceso de envio finalizado correctamente \n");
+	return 0;
+}
+
+//Recibe datos del host
+int Cliente::recibir(char* buffer,int len) {
+	int resultado=recv(ConnectSocket,buffer,len,0);
+	return resultado;
+}
+
+//Termina la conexion con el host
+int Cliente::desconectar() {
+	closesocket(ConnectSocket);
+	WSACleanup();
+	printf("Conexion finalizada \n");
+	return 0;
+}
--- Diablo/Servidor/server.cpp
+++ Diablo/Servidor/server.cpp
@@ -1,132 +1,35 @@
-#include <winsock2.h>
-#include <ws2tcpip.h>
+#include "servidor.h"
 #include <stdio.h>
 
-#pragma comment(lib, "Ws2_32.lib")
-
-#define DEFAULT_PORT "27015"
+#define DEFAULT_PORT "27018"
 #define DEFAULT_BUFLEN 512
 
-
 int main() {
-	WSADATA wsaData;
+	Servidor servidor;
 	int resultado;
-
-	//Inicializa Winsock
-	resultado=WSAStartup(MAKEWORD(2,2),&wsaData);
-	if (resultado!=0) {
-		printf("Inicializacion fallida: %d \n",resultado);
-		return 1;
-	}
-	printf("Winsock inicializado \n");
+	servidor.generarDireccion(NULL,DEFAULT_PORT);
+	servidor.crearSocket();
+	servidor.escuchar();
+	servidor.aceptarConexion();
 	
-	//Genera la address local
-	struct addrinfo *result = NULL, *ptr = NULL, hints;
-
-	ZeroMemory(&hints, sizeof (hints));
-	hints.ai_family = AF_INET;
-	hints.ai_socktype = SOCK_STREAM;
-	hints.ai_protocol = IPPROTO_TCP;
-	hints.ai_flags = AI_PASSIVE;
-
-	//Obtiene la address
-	resultado = getaddrinfo(NULL, DEFAULT_PORT, &hints, &result);
-	if (resultado != 0) {
-		printf("Obtencion de address fallida: %d\n", resultado);
-		WSACleanup();
-		return 1;
-	}
-	printf("Address local generada \n");
-
-	//Creo el socket
-	SOCKET ListenSocket = INVALID_SOCKET;
-	ListenSocket = socket(result->ai_family, result->ai_socktype, result->ai_protocol);
-	if (ListenSocket == INVALID_SOCKET) {
-		("Error al crear socket: %ld\n", WSAGetLastError());
-		freeaddrinfo(result);
-		WSACleanup();
-		return 1;
-	}
-	printf("Socket creado \n");
-
-	//Configura el socket para recibir conexiones
-	resultado = bind( ListenSocket, result->ai_addr, (int)result->ai_addrlen);
-    if (resultado == SOCKET_ERROR) {
-        printf("Conexion con cliente fallida: %d\n", WSAGetLastError());
-        freeaddrinfo(result);
-        closesocket(ListenSocket);
-        WSACleanup();
-        return 1;
-    }
-	freeaddrinfo(result); //Ya no es necesaria si la configuracion fue establecida con exito
-	printf("Socket configurado \n");
-
-	//Verifica si hay movimiento en la cola de conexiones
-	if ( listen( ListenSocket, SOMAXCONN ) == SOCKET_ERROR ) {
-		printf( "Deteccion de movimiento en cola fallida: %ld\n", WSAGetLastError() );
-		closesocket(ListenSocket);
-		WSACleanup();
-		return 1;
-	}
-	printf("Esperando una conexion \n");
-
-	//Acepta una conexion
-	SOCKET ClientSocket;
-
-	ClientSocket = INVALID_SOCKET;
-
-	ClientSocket = accept(ListenSocket, NULL, NULL);
-	if (ClientSocket == INVALID_SOCKET) {
-		printf("Aceptar conexion fallida: %d\n", WSAGetLastError());
-		closesocket(ListenSocket);
-		WSACleanup();
-		return 1;
-	}
-	printf("Conexcion aceptada \n");
-
 	//Envio/Recepcion de datos
-	char recvbuf[DEFAULT_BUFLEN];
+	char buffer[DEFAULT_BUFLEN];
 	int resulEnvio;
-	int recvbuflen = DEFAULT_BUFLEN;
 
 	// Recibir hasta que el cliente cierre la conexion
 	do {
-		resultado = recv(ClientSocket, recvbuf, recvbuflen, 0);
-		if (resultado > 0) {
-			printf("Bytes recibidos: %d\n", resultado);
-			printf("El mensaje recibido es: %s\n",recvbuf);
+		resultado = servidor.recibir(buffer,strlen(buffer));
+		if (resultado > 0) {			
 			//Devuelve el buffer al cliente
-			resulEnvio = send(ClientSocket, recvbuf, resultado, 0);
-			if (resulEnvio == SOCKET_ERROR) {
-				printf("Envio de buffer fallido: %d\n", WSAGetLastError());
-				closesocket(ClientSocket);
-				WSACleanup();
-				return 1;
-			}
-			//printf("Bytes enviados: %d\n", resulEnvio);
-		} else if (resultado == 0)
-			printf("Cerrando conexion...\n");
-		else {
-			printf("Recepcion de datos fallida: %d\n", WSAGetLastError());
-			closesocket(ClientSocket);
-			WSACleanup();
+			resulEnvio=servidor.enviar(buffer,resultado);
+			if (resulEnvio==1) servidor.cerrarSocket();
+		} else if (resultado < 0) {
+			servidor.cerrarSocket();
 			return 1;
 		}
-
 	} while (resultado > 0);
 
-	//Cierra el modo de envio
-	resultado = shutdown(ClientSocket, SD_SEND);
-	if (resultado == SOCKET_ERROR) {
-		printf("Cierre modo envio fallido: %d\n", WSAGetLastError());
-		closesocket(ClientSocket);
-		WSACleanup();
-		return 1;
-	}
-
-	//Cierra el socket
-	closesocket(ClientSocket);
-	WSACleanup();
+	servidor.cerrarSocket();
 
 	getchar();
 	return 0;

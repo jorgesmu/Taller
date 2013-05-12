4ea2adc1091e850e48ffff14f626d55abd7b27fb
jmartty <jmartty@hotmail.com>
2013-05-12 18:31:40 -0300

    PlayerManager

---
+++ Diablo/Servidor/playerman.cpp
@@ -0,0 +1,90 @@
+#include "playerman.h"
+#include "../source/utilities/aux_func.h"
+
+#include <exception>
+#include <cassert>
+
+Player::Player() {
+	nick = "uninit-nick-pm";
+	tipo_personaje = "uninit-pje";
+	isOnline = false;
+	x = y = 0;
+}
+
+Player::~Player() {
+	// Nada
+}
+
+void Player::init(const std::string& nick, const std::string& pje) {
+	this->nick = nick;
+	this->tipo_personaje = pje;
+}
+
+void Player::setPos(int x, int y) {
+	this->x = x;
+	this->y = y;
+}
+
+int Player::getX() const {
+	return x;
+}
+
+int Player::getY() const {
+	return y;
+}
+
+bool Player::isOn() const {
+	return this->isOnline;
+}
+
+void Player::setOffline() {
+	isOnline = false;
+}
+
+void Player::setOnline() {
+	isOnline = true;
+}
+
+
+//////////////
+
+
+// Devuelve si un player existe
+bool PlayerManager::playerExists(const std::string& nick) const {
+	return player_map.find(nick) != player_map.end();
+}
+
+// Agrega un jugador
+void PlayerManager::addPlayer(const std::string& nick, const std::string& tipo_pje, Mapa& mapa) {
+	// Chequeo de sanidad
+	assert(!playerExists(nick));
+	Player& p = player_map[nick];
+	p.init(nick, tipo_pje);
+	// Buscar posicion en el mapa
+	bool found = false;
+	int x = 0; // Contador de iteraciones
+	int w, h; // Para guardar el tamaño del mapa
+	mapa.getSize(&w, &h);
+	int rand_x, rand_y;
+	// Limitamos la cantidad de iteraciones
+	while(!found && x < 1000) {
+		rand_x = intRand(0, w);
+		rand_y = intRand(0, h);
+		if(mapa.getTile(rand_x, rand_y)->isCaminable()) {
+			found = true;
+		}
+		x++;
+	}
+	if(!found) {
+		throw std::runtime_error("No se pudo ubicar el personaje\n");
+	}else{
+		p.setOnline();
+		p.setPos(rand_x, rand_y);
+	}
+}
+
+// Devuelva un jugador
+Player& PlayerManager::getPlayer(const std::string& nick) {
+	assert(playerExists(nick));
+	return player_map[nick];
+}
---
+++ Diablo/Servidor/playerman.h
@@ -0,0 +1,41 @@
+#pragma once
+
+#include <string>
+#include <map>
+
+#include "../source/display/mapa.h"
+
+class Player {
+	private:
+	std::string nick;
+	std::string tipo_personaje;
+	bool isOnline;
+	int x, y; // Pos en tiles logicos en el mapa
+	
+	public:
+	Player();
+	~Player();
+	void init(const std::string& nick, const std::string& pje);
+	void setPos(int x, int y);
+	int getX() const;
+	int getY() const;
+	bool isOn() const;
+	void setOffline();
+	void setOnline();
+};
+
+class PlayerManager {
+	private:
+	// Hash con los jugadores que entraron al servidor
+	std::map<std::string, Player> player_map;
+
+	public:
+	// Devuelve si un player existe
+	bool playerExists(const std::string& nick) const;
+	// Agrega un jugador
+	// Inicializa en una posicion aleatoria del mapa
+	void addPlayer(const std::string& nick, const std::string& tipo_pj, Mapa& mapa);
+	// Devuelve un jugador
+	Player& getPlayer(const std::string& nick);
+
+};

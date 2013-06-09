a40763413297c21547d03d7dfe0831f2aacbb354
Jorge Smulevici <jorgsmu@gmail.com>
2013-06-09 15:52:09 -0300

    Enemigo server y golem.

---
+++ Diablo/Servidor/golem.h
@@ -0,0 +1,12 @@
+#pragma once
+#include "enemigoServer.h"
+#include <string>
+using namespace std;
+class Golem:public Enemigo{
+	private:
+		string duenio;
+	public:
+	Golem(string nickDuenio);
+	bool personaje_en_radio(MapaServidor& mapa,PlayerManager& pm);
+
+};
